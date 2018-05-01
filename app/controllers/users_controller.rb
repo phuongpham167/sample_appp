442ac1d70b2795159a1a6aa32785b431b368f792
Phuong Pham <phuongptt.uet@gmail.com>
2018-05-01 12:43:56 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id:params[:id]
+    return if @user
+    flash[:success] = t "static_pages.help.usernotfound"
+    redirect_to :help
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "static_pages.home.h1"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
   end
 end
