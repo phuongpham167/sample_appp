a6f6dc38a5320bc1e9b35ff9bddadec3943c1522
Phuong Pham <phuongptt.uet@gmail.com>
2018-05-04 14:00:25 +0700

    Finish edi, update, destroy user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
 
-  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     base_title = I18n.t 'helpers.application.base_title'
     if page_title.empty?
