a796b4e74cef7bb7faf094b5a6644d7e6aa22079
Phuong Pham <phuongptt.uet@gmail.com>
2018-05-04 14:00:25 +0700

    Finish edi, update, destroy user

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 gem "rails",        "5.1.4"
 gem "bcrypt",         "3.1.11"
 gem "bootstrap-sass", "3.3.7"
+gem "faker",          "1.7.3"
 gem "puma",         "3.9.1"
 gem "sass-rails",   "5.0.6"
 gem "uglifier",     "3.2.0"
@@ -16,6 +17,8 @@ gem "jquery-rails", "4.3.1"
 gem "turbolinks",   "5.0.1"
 gem "jbuilder",     "2.7.0"
 gem "i18n-js"
+gem "will_paginate", "3.1.6"
+gem "bootstrap-will_paginate", "1.0.0"
 
 group :development, :test do
   gem "sqlite3", "1.3.13"
@@ -23,17 +26,17 @@ group :development, :test do
 end
 
 group :development do
-  gem "web-console",           "3.5.1"
-  gem "listen",                "3.1.5"
-  gem "spring",                "2.0.2"
+  gem "web-console", "3.5.1"
+  gem "listen", "3.1.5"
+  gem "spring", "2.0.2"
   gem "spring-watcher-listen", "2.0.1"
 end
 
 group :test do
   gem "rails-controller-testing", "1.0.2"
-  gem "minitest-reporters",       "1.1.14"
-  gem "guard",                    "2.13.0"
-  gem "guard-minitest",           "2.4.4"
+  gem "minitest-reporters", "1.1.14"
+  gem "guard", "2.13.0"
+  gem "guard-minitest", "2.4.4"
 end
 
 group :production do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,11 +11,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-
   # Returns the hash digest of the given string.
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -36,4 +36,13 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
